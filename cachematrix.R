aaeefd6105e0c2b02ca3270182b350ddd86367f1
Trevor Popovits <trevpopovits@gmail.com>
2015-02-22 18:06:16 -0600

    explaining

--- cachematrix.R
+++ cachematrix.R
@@ -18,8 +18,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Grabbed some code from the writeup, for cachemean function. Need to modify it
+## in order to solve the matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 	  m <- x$getmean()
